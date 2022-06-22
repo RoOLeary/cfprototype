1c532bcc7322326d8f8f2600ea33657d00f075e7
ronan <ronan@thenextweb.com>
2022-06-22 13:42:57 +0200

    fix rendering of generic list pages

--- components/post.js
+++ components/post.js
@@ -5,7 +5,7 @@ export default function Post(post) {
   return (
     <article>
       <h2>{post[1].title}</h2>
-      <div dangerouslySetInnerHTML={{__html: post[1].content[0].content }} />
+      {/* <div dangerouslySetInnerHTML={{__html: post[1].content[0].content }} /> */}
       <Link href={`/posts/${post[1].slug}`}>
         <a>Read more...</a>
       </Link>
