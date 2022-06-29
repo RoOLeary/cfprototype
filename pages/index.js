46a7eb13d70800ce097251a0da2c20bd25a82c44
ronan <ronan@thenextweb.com>
2022-06-29 14:39:26 +0200

    test fallback

--- pages/index.js
+++ pages/index.js
@@ -7,21 +7,42 @@ import styles from '../styles/Home.module.css';
 
 const fetcher = (...args) => fetch(...args).then(res => res.json())
 console.log(fetcher);
+// export async function getStaticProps() {
+//   // fetch list of posts
+//   const response = await fetch(
+//     'https://api2.tnw-staging.com/v2/articles?limit=20'
+//   )
+//   const postList = await response.json()
+//   return {
+//     props: {
+//       postList,
+//     },
+//   }
+// }
+
+
 
 export default function Home() {
 
   const [page, setPage] = useState(1);
   const { data, error } = useSWR(
     `https://api2.tnw-staging.com/v2/articles?page=${page}&limit=10`,
-    fetcher, { refreshInterval: 1000 }
+    fetcher
   );
   
+  // console.log(data);
+
   if (error) return <h1>Error</h1>;
   // if (!data) return <h1>Loading...</h1>;
 
+
+
   return (
     <Layout>
+      
+     
       <section className="b-text  c-section" id="learn-more">
+          
           <div className="o-wrapper">
           {!data ? <h1 className={'b-text__heading'}>Loading...</h1> :
             <div>
--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -17,7 +17,7 @@ export async function getStaticPaths() {
         },
       }
     }),
-    fallback: false
+    fallback: true
   }
 }
 
