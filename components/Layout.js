0ad4536e9ba6ae058af4e01d71919418565be489
ronan <ronan@thenextweb.com>
2022-06-29 12:56:35 +0200

    add framer motion

--- components/Layout.js
+++ components/Layout.js
@@ -2,6 +2,17 @@ import Head from 'next/head';
 import Nav from './Nav';
 import SEO from './SEO';
 import Footer from './Footer';
+
+import { motion } from 'framer-motion';
+import { AnimatePresence } from 'framer-motion';
+const variants = {
+    hidden: { opacity: 0, x: 0, y: 0 },
+    enter: { opacity: 1, x: 0, y: 0 },
+    exit: { opacity: 0, x: 0, y: 0 },
+}
+
+
+
 const Layout = ({ children }) => {
 
     return(
@@ -10,9 +21,16 @@ const Layout = ({ children }) => {
                 <SEO title="Next"/>
             </Head>
             <Nav />
-            <main>
+            <motion.main
+                variants={variants} // Pass the variant object into Framer Motion 
+                initial="hidden" // Set the initial state to variants.hidden
+                animate="enter" // Animated state to variants.enter
+                exit="exit" // Exit state (used later) to variants.exit
+                transition={{ type: 'linear' }} // Set the transition to linear
+                className="body__content"
+            >
                 {children}
-            </main>
+            </motion.main>
             <Footer />
         </>
 
