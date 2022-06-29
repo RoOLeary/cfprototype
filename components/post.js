35236c574931aeece9d048d5c778eecdb2a9dfa0
ronan <ronan@thenextweb.com>
2022-06-29 08:35:36 +0200

    fix

--- components/post.js
+++ components/post.js
@@ -1,6 +1,9 @@
 import Link from 'next/link'
-import Image from 'next/image'
+// import Image from 'next/image'
 import styles from '../styles/Post.module.css'
+
+
+
 
 export default function Post(post) {
   // console.log(post[1].media[0].media.attributes.url);
@@ -9,7 +12,7 @@ export default function Post(post) {
   return (
     <article className={styles.articleStyles}>
       <div className={styles.articleFlex}>
-      <Image src={post[1].media[0].media.attributes.url} width={'300'} height={'200'} />
+      {/* <Image loader={myLoader} src={post[1].media[0].media.attributes.url} width={'300'} height={'200'} /> */}
       <Link href={`/posts/${slug}`} scroll={false}><a><h2 className={styles.artMarg}>{title}</h2></a></Link>
       <br></br>
       </div>
--- next.config.js
+++ next.config.js
@@ -1,31 +1,42 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: false,
-    trailingSlash: false,
-    distDir: '/out',
-  }
-  
-  module.exports = nextConfig
-  
-  module.exports = {
-    images: {
-        domains: ['cdn0.tnwcdn.com'],
-    },
-    async headers() {
-      return [
-        {
-          source: '/all-components',
-          headers: [
-            {
-              key: 'x-custom-header',
-              value: 'my custom header value',
-            },
-            {
-              key: 'x-another-custom-header',
-              value: 'my other custom header value',
-            },
-          ],
+  reactStrictMode: false,
+  trailingSlash: false,
+  distDir: '/out',
+}
+
+module.exports = nextConfig
+
+
+
+
+module.exports = {
+
+        exportPathMap: async function (
+            defaultPathMap,
+            { dev, dir, outDir, distDir, buildId }
+        ) {
+            return {
+            '/': { page: '/' },
+            '/about': { page: '/about' },
+            '/posts': { page: '/posts'}
+            }
         },
-      ]
-    },
-  }
+  async headers() {
+    return [
+      {
+        source: '/all-components',
+        headers: [
+          {
+            key: 'x-custom-header',
+            value: 'my custom header value',
+          },
+          {
+            key: 'x-another-custom-header',
+            value: 'my other custom header value',
+          },
+        ],
+      },
+    ]
+  },
+}
