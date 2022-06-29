b3bc137fb274eec0afbea5d5d9ccedc81ca156a2
ronan <ronan@thenextweb.com>
2022-06-29 11:47:51 +0200

    thought I deployed this already

--- pages/index.js
+++ pages/index.js
@@ -7,20 +7,6 @@ import styles from '../styles/Home.module.css';
 
 const fetcher = (...args) => fetch(...args).then(res => res.json())
 console.log(fetcher);
-// export async function getStaticProps() {
-//   // fetch list of posts
-//   const response = await fetch(
-//     'https://api2.tnw-staging.com/v2/articles?limit=20'
-//   )
-//   const postList = await response.json()
-//   return {
-//     props: {
-//       postList,
-//     },
-//   }
-// }
-
-
 
 export default function Home() {
 
@@ -30,19 +16,12 @@ export default function Home() {
     fetcher
   );
   
-  // console.log(data);
-
   if (error) return <h1>Error</h1>;
   // if (!data) return <h1>Loading...</h1>;
 
-
-
   return (
     <Layout>
-      
-     
       <section className="b-text  c-section" id="learn-more">
-          
           <div className="o-wrapper">
           {!data ? <h1 className={'b-text__heading'}>Loading...</h1> :
             <div>
@@ -55,21 +34,19 @@ export default function Home() {
           </div>
           <div className="o-wrapper">
           {page > 1 ?
-          <button
-          // disabled={}
-          className={'c-button'}
-          onClick={() => setPage(page - 1)}
-        >
-          Previous
-        </button> : ''}
+          <button 
+            className={'c-button'}
+            onClick={() => setPage(page - 1)}
+          >
+            Previous
+          </button> : ''}
           
-        <button
-          // disabled={!data.next}
-          className={'c-button'}
-          onClick={() => setPage( page + 1)}
-        >
-          Next
-        </button>
+          <button
+            className={'c-button'}
+            onClick={() => setPage( page + 1)}
+          >
+            Next
+          </button>
         </div>
       </section>
     </Layout>
