4111b53ba1b02d5fefcfe10436a8b1e737a3a482
ronan <ronan@thenextweb.com>
2022-07-01 10:15:41 +0200

    attempt build with server side gen pages

--- pages/index.js
+++ pages/index.js
@@ -6,36 +6,36 @@ import useSWR from "swr";
 import styles from '../styles/Home.module.css';
 import { useRouter } from 'next/router'
 
-const fetcher = (...args) => fetch(...args).then(res => res.json())
-console.log(fetcher);
-// export async function getStaticProps() {
-//   // fetch list of posts
-//   const response = await fetch(
-//     'https://api2.tnw-staging.com/v2/articles?limit=20'
-//   )
-//   const postList = await response.json()
-//   return {
-//     props: {
-//       postList,
-//     },
-//   }
-// }
+// const fetcher = (...args) => fetch(...args).then(res => res.json())
+// console.log(fetcher);
+export async function getStaticProps() {
+  // fetch list of posts
+  const response = await fetch(
+    'https://api2.tnw-staging.com/v2/articles?limit=20'
+  )
+  const postList = await response.json()
+  return {
+    props: {
+      postList,
+    },
+  }
+}
 
 
 
-export default function Home() {
+export default function Home({ postList }) {
 
   const router = useRouter();
   const [title, setPageTitle] = useState('Latest Posts')
   const [page, setPage] = useState(1);
-  const { data, error } = useSWR(
-    `https://api2.tnw-staging.com/v2/articles?page=${page}&limit=10`,
-    fetcher
-  );
+  // const { data, error } = useSWR(
+  //   `https://api2.tnw-staging.com/v2/articles?page=${page}&limit=10`,
+  //   fetcher
+  // );
   
   // console.log(data);
 
-  if (error) return <h1>Error</h1>;
+  // if (error) return <h1>Error</h1>;
   // if (!data) return <h1>Loading...</h1>;
 
   const messyNav = (page) => {
@@ -51,11 +51,11 @@ export default function Home() {
       <section className="b-text  c-section" id="learn-more">
           
           <div className="o-wrapper">
-          {!data ? <h1 className={'b-text__heading'}>Loading...</h1> :
+          {!postList ? <h1 className={'b-text__heading'}>Loading...</h1> :
             <div>
             <h1 className={'b-text__heading'}>{title}</h1>
             <br />
-            {Object.entries(data).map((post, idx) => (
+            {Object.entries(postList).map((post, idx) => (
               <Post {...post} key={idx} />
             ))}
             </div> }
--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -5,33 +5,43 @@ import { useEffect } from 'react';
 import Layout from '../../components/Layout'
 import styles from '../../styles/Home.module.css'
 
-export async function getStaticPaths() {
-  const response = await fetch(
-    'https://api2.tnw-staging.com/v2/articles?limit=350'
-  )
-  const postList = await response.json()
-  return {
-    paths: Array.from(postList).map((post) => {
-      return {
-        params: {
-          slug: `${post.slug}`,
-        },
-      }
-    }),
-    fallback: false
-  }
-}
+// export async function getStaticPaths() {
+//   const response = await fetch(
+//     'https://api2.tnw-staging.com/v2/articles?limit=350'
+//   )
+//   const postList = await response.json()
+//   return {
+//     paths: Array.from(postList).map((post) => {
+//       return {
+//         params: {
+//           slug: `${post.slug}`,
+//         },
+//       }
+//     }),
+//     fallback: false
+//   }
+// }
 
 
-export async function getStaticProps({ params }) {
-  // fetch single post detail
-  const response = await fetch(
-    `https://api2.tnw-staging.com/v2/articles/${params.slug}`
-  )
-  const post = await response.json()
-  return {
-    props: post[0],
-  }
+// export async function getStaticProps({ params }) {
+//   // fetch single post detail
+//   const response = await fetch(
+//     `https://api2.tnw-staging.com/v2/articles/${params.slug}`
+//   )
+//   const post = await response.json()
+//   return {
+//     props: post[0],
+//   }
+// }
+
+export async function getServerSideProps(context) {
+  console.log(context.query.slug);
+  // Fetch data from external API
+  const res = await fetch(`https://api2.tnw-staging.com/v2/articles/${context.query.slug}`)
+  const post = await res.json()
+
+  // Pass data to the page via props
+  return { props: post[0] }
 }
 
 export default function Post( post ) {
