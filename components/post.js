86bcaa49fbbc3e7fae9ab377b596c2aacc8075e6
ronan <ronan@thenextweb.com>
2022-07-01 10:24:16 +0200

    update ssr post

--- components/post.js
+++ components/post.js
@@ -31,7 +31,7 @@ export default function Post(post) {
         <img src={imgSrc} width={300} height={200} />
         
         <div className={styles.innerFlex}>
-          <a href={`/posts/${slug}`}><h2 className={styles.artMarg}>{title.replace(/<[^>]+>/g, '')}</h2></a>
+          <Link href={`/posts/${slug}`}><a><h2 className={styles.artMarg}>{title.replace(/<[^>]+>/g, '')}</h2></a></Link>
           <br />
           <p className={styles.artMarg}>{post[1].properties.published} - {post[1].authors[0].name} <br/>
             <div>Tags:
