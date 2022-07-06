4a87a277c3f79de576ed7012701b25cc52e67f9f
ronan <ronan@thenextweb.com>
2022-07-06 12:55:07 +0200

    see if this works on cf

--- pages/authors/[slug].js
+++ pages/authors/[slug].js
@@ -2,15 +2,13 @@ import Layout from '../../components/Layout'
 import { useRouter } from 'next/router';
 
 export default function Author() {
-
     const router = useRouter(); 
     const authorName = router.query.slug;
-
     return (
         <Layout>
             <section className={'b-text c-section'}>
                 <div className={'o-wrapper'}>
-                <h1>Author: {authorName.charAt(0).toUpperCase() + authorName.slice(1)}</h1>
+                <h1>Author: {authorName ? authorName.charAt(0).toUpperCase() + authorName.slice(1) : 'Author' }</h1>
                 </div>
             </section>
         </Layout>
--- pages/index.js
+++ pages/index.js
@@ -2,24 +2,13 @@ import Head from 'next/head';
 import Layout from '../components/Layout';
 import React, { useState } from "react";
 import Post from '../components/post';
-import useSWR from "swr";
+import useSWRInfinite from "swr/infinite";
 import styles from '../styles/Home.module.css';
 import { useRouter } from 'next/router'
 
 const fetcher = (...args) => fetch(...args).then(res => res.json())
 console.log(fetcher);
-// export async function getStaticProps() {
-//   // fetch list of posts
-//   const response = await fetch(
-//     'https://api2.tnw-staging.com/v2/articles?limit=20'
-//   )
-//   const postList = await response.json()
-//   return {
-//     props: {
-//       postList,
-//     },
-//   }
-// }
+const PAGE_SIZE = 10;
 
 
 
@@ -27,16 +16,23 @@ export default function Home() {
 
   const router = useRouter();
   const [title, setPageTitle] = useState('Latest Posts')
-  const [page, setPage] = useState(1);
-  const { data, error } = useSWR(
-    `https://api2.tnw-staging.com/v2/articles?page=${page}&limit=10`,
-    fetcher
+  const { data, error, mutate, size, setSize, isValidating } = useSWRInfinite(
+    index =>
+      `https://api2.tnw-staging.com/v2/articles?page=${index +
+        1}&limit=${PAGE_SIZE}`,
+    fetcher,
   );
   
-  // console.log(data);
+    
 
-  if (error) return <h1>Error</h1>;
-  // if (!data) return <h1>Loading...</h1>;
+  const posts = data ? [].concat(...data) : [];
+  const isLoadingInitialData = !data && !error;
+  const isLoadingMore = isLoadingInitialData || (size > 0 && data && typeof data[size - 1] === "undefined");
+  const isEmpty = data?.[0]?.length === 0;
+  const isReachingEnd = isEmpty || (data && data[data.length - 1]?.length < PAGE_SIZE);
+  const isRefreshing = isValidating && data && data.length === size
+
+  console.log(posts);
 
   const messyNav = (page) => {
     setPage( page + 1 );
@@ -55,27 +51,23 @@ export default function Home() {
             <div>
             <h1 className={'b-text__heading'}>{title}</h1>
             <br />
-            {Object.entries(data).map((post, idx) => (
-              <Post {...post} key={idx} />
+            {Object.entries(posts).map((post, idx) => (
+                <Post {...post} key={idx} />
             ))}
             </div> }
           </div>
           <div className="o-wrapper">
-          {page > 1 ?
-          <button 
-            className={'c-button'}
-            onClick={() => setPage(page - 1)}
-          >
-            Previous
-          </button> : ''}
-          
-          <button
-            className={'c-button'}
-            onClick={() => messyNav(page)}
-          >
-            Next
-          </button>
-        </div>
+            <button
+              className={`bg-black hover:bg-gray-700 text-white font-bold py-2 px-4 rounded`}
+              disabled={isLoadingMore || isReachingEnd}
+              onClick={() => setSize(size + 1)}>
+              {isLoadingMore
+                  ? 'Loading...'
+                  : isReachingEnd
+                      ? 'No More Posts'
+                      : 'Load More'}
+            </button>
+          </div>
       </section>
     </Layout>
   )
