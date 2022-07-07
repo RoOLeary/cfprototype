f453ab10a15b658b258929a2c640851c0e017cde
ronan <ronan@thenextweb.com>
2022-07-07 12:40:33 +0200

    see if this works

--- pages/topic/[tag].tsx
+++ pages/topic/[tag].tsx
@@ -1,40 +1,48 @@
 import Link from "next/link"
 import { useRouter } from "next/router"
 import Layout from '../../components/Layout'
+import { GetServerSideProps } from 'next';
 
-export default function Tag(tag) {
-
-    const router = useRouter(); 
-    const tagName = router.query.tag ? router.query.tag : 'tagName';
-
-    console.log(tagName);
+export default function Tag(tag){
     
+    const router = useRouter(); 
+    let tagName = router.query.tag ? router.query.tag : 'tagName';
+    console.log('tagname: ' + tagName)
     return (
         <Layout>
             <section className={'b-text c-section'}>
                 <div className={'o-wrapper'}>
-                    <h1>Tag</h1>
-                    <p>List of articles tagged with {tagName}</p>
+                    <h1>Tag: {tagName ? tagName : 'Tag' }</h1>
+                    <br />
+                    <p>List of articles tagged with <strong><em>{tagName}</em></strong></p>
+                    <br />
+                    <ul>
+                        {tag.tagged.map((tag, i) => {
+                            console.log(tag);
+                            return <li key={tag.slug}><Link href={`/posts/${tag.slug}`}><a>{tag.title}</a></Link></li>
+                        })}
+                    </ul>
+                    <Link href={'/'}><a>Back</a></Link>
                 </div>
-                <Link href={'/'}><a>Back</a></Link>
             </section>
         </Layout>
        
     )
 }
 
-// export async function getStaticPaths() {
+
+// export const getStaticPaths: GetStaticPaths = async () => {
 //     const response = await fetch(
-//       'https://api2.tnw-staging.com/v2/tags?limit=300'
+//       'https://api2.tnw-staging.com/v2/articles?limit=350'
 //     )
-    
 //     const tagList = await response.json()
 //     // console.log(tagList);
 //     return {
 //       paths: Array.from(tagList).map((tag) => {
+        
 //         return {
 //           params: {
-//             tag: `${tag.slug}`,
+//             tag: `${tag['slug']}`,
 //           },
 //         }
 //       }),
@@ -43,14 +51,17 @@ export default function Tag(tag) {
 //   }
   
   
-//   export async function getStaticProps({ params }) {
-//     // fetch single post detail
-//     console.log(params.tag);
-//     const response = await fetch(
-//       `https://api2.tnw-staging.com/v2/tags/${params.tag}`
-//     )
-//     const tag = await response.json()
-//     return {
-//       props: tag[0],
-//     }
-//   }
+export const getServerSideProps: GetServerSideProps = async ({ params }) => {
+   
+    const response = await fetch(
+      `https://api2.tnw-staging.com/v2/articles?tag[]=${params.tag}`
+    )
+    
+    const tag = await response.json()
+    
+    return {
+      props: { tagged: tag },
+    }
+  }
+
+
