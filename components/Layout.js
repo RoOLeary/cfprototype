afe2dc6fcaad28c8714c50489e92d0846f84e58c
Ronan <roleary81@gmail.com>
2022-07-09 17:02:53 +0200

    test article router

--- components/Layout.js
+++ components/Layout.js
@@ -8,9 +8,9 @@ import { motion } from 'framer-motion';
 import { useRouter } from 'next/router';
 
 const variants = {
-    hidden: { opacity: 0 },
-    enter: { opacity: 1 },
-    exit: { opacity: 0 },
+    hidden: { opacity: 0, x: 0, y: 0 },
+    enter: { opacity: 1, x: 0, y: 0 },
+    exit: { opacity: 0, x: 0, y: 0 },
 }
 
 
