daec6b02092c9f08cabb8315a3e9cca3c82c4e03
ronan <ronan@thenextweb.com>
2022-07-12 13:41:02 +0200

    typescriptify moar of the things

--- components/PageBlocks.tsx
+++ components/PageBlocks.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Children, PropsWithChildren } from 'react';
 import Header from './Header';
 import Hero from './Hero';
 import Text from './Text';
@@ -10,43 +10,59 @@ import Speakers from './Speakers';
 import { any } from 'prop-types';
 // import Projects from './Projects';
 
-const PageBlocks = ({ content }: any): JSX.Element[] => {
+interface IProps {
+    content: PropsWithChildren
+  }
 
-    const pageBlocks = Array.from(content);
-    const pageBlocksList = pageBlocks.map((block, id) => {
-           
-        switch(block['blockType']) {
+
+const renderContent = ( content ) => {
+    console.log('in render method');
+    const pageBlocksList = Object.entries(content).map((block: any, id: number) => {
+    const blockContent = block[1];
+        switch(block[1]['blockType']) {
             case 'header':
-                return <Header key={block['uid']} content={block} />
+                return <Header key={block['uid']} content={blockContent} />
             case 'hero':
-                return <Hero key={block['uid']} content={block} />
+                return <Hero key={block['uid']} content={blockContent} />
             case 'text':
-                return <Text key={block['uid']} content={block} />
+                return <Text key={block['uid']} content={blockContent} />
             case 'textVisual':
-                return <TextVisual key={block['uid']} content={block} />
+                return <TextVisual key={block['uid']} content={blockContent} />
             case 'imageSlider':
-                return <Slider key={block['uid']} content={block} />
+                return <Slider key={block['uid']} content={blockContent} />
             case 'faq':
-                return <Faq key={block['uid']} content={block} />
+                return <Faq key={block['uid']} content={blockContent} />
             case 'video':
-                return <Video key={block['uid']} content={block} />
+                return <Video key={block['uid']} content={blockContent} />
             case 'speakers':
-                return <Speakers key={block['uid']} content={block} />
+                return <Speakers key={block['uid']} content={blockContent} />
             // case 'tickets':
-            //     return <Tickets key={block['uid']} content={block} />
+            //     return <Tickets key={block['uid']} content={blockContent} />
             default:
                 return(
-                    <div key={id}>
-                        <h3>{block['blockType']}</h3>
-                        <pre>{JSON.stringify(block, null, 2)}</pre>
-                    </div>
+                    <>
+                        <div key={id}>
+                            <h3>{block[1]['blockType']}</h3>
+                            <pre>{JSON.stringify(block, null, 2)}</pre>
+                        </div>
+                    </>
                 )
         }
         
     })
 
     return pageBlocksList;
+}
 
+
+
+
+const PageBlocks = ({ content }) => {
+    return(
+        <>
+            {renderContent(content)}
+        </>
+    )
 } 
 
 export default PageBlocks;
