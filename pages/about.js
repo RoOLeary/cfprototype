64d2fcbcd8b3c736f1685da6f696ead716ab50c7
ronan <ronan@thenextweb.com>
2022-07-12 10:23:36 +0200

    updates blocks and about

--- pages/about.js
+++ pages/about.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect, useMemo } from "react";
 import Layout from '../components/Layout'
 import Header from '../components/Header'
 import PageBlocks from '../components/PageBlocks'
-// import { GetStaticProps } from 'next';
+
 
 export default function About({ entry }) {
   // console.log(slug);
@@ -18,27 +18,16 @@ export default function About({ entry }) {
   )
 }
 
-// export const getStaticProps = async (context) => {
-//   const slug = context?.params?.slug || "about";
-//   const res = await fetch(`https://servd-test-staging.cl-eu-west-3.servd.dev/api/pages/${slug}.json`);
-//   let entry = await res.json();
-
-//   return {
-//       props: {
-//           entry: entry
-//       }
-//   }
-// }
-
 export const getStaticProps = async ({params}) => {
-    const slug = params?.slug || "about";
-    const response = await fetch(
-      `https://cities.thenextweb.com/api/pages/${slug}.json`
-    )
-    const entry = await response.json();
-    return {
-      props: { 
-        entry: entry
-      },
-    }
+  const slug = params?.slug || "about";
+  const response = await fetch(
+    `https://cities.thenextweb.com/api/pages/${slug}.json`
+  )
+  const entry = await response.json();
+  return {
+    props: { 
+      entry: entry
+    },
+  }
 }
+
