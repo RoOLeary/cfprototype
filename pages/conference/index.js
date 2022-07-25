35293c4669493e9a0e6b7c305d5c61876f8fbc82
ronan <ronan@thenextweb.com>
2022-07-25 17:02:07 +0200

    try again

--- pages/conference/index.js
+++ pages/conference/index.js
@@ -17,7 +17,7 @@ const TextText = {
 
 const videoEmbedCode = `<iframe width="560" height="315" src="https://www.youtube.com/embed/S1-wuoFsdT4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
 
-export default function Conference() {
+export default function Index() {
   return (
     <Layout>
       <Hero content={ConfText} />
