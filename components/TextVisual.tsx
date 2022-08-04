3feeada491a36c2f750c2669348229e3dcc1f747
Ronan <roleary81@gmail.com>
2022-08-04 13:16:36 +0200

    update textVisual component

--- components/TextVisual.tsx
+++ components/TextVisual.tsx
@@ -3,23 +3,26 @@ import Link from 'next/link'
 // import styles from '../styles/TextVisual.module.css';
 
 type Props = {
-    content: ReactNode;
+    content: any;
 }
 
 const TextVisual = ({ content }: Props): JSX.Element => {
-    
+    console.log(content);
+
+    const { textVisualHeading, textVisualContent, textVisualImage } = content;
+
     return(
     <section className="b-textImage b-textImage--tint b-textVisual js-equinoxNode t-dark is-visible">
         <div className="b-textImage__imageContainer">
             <figure className="b-textImage__image">
-                <img alt={content.textVisualHeading} className="b-textImage__imageImg b-textImage__imageImg--background" src={content.textVisualImage} />
+                <img alt={textVisualHeading} className="b-textImage__imageImg b-textImage__imageImg--background" src={textVisualImage} />
             </figure>
         </div>
 
         <div className="o-wrapper">
             <div className="b-textImage__text">
-                <h2 className="b-textImage__heading">{content.textVisualHeading}</h2>
-                <p>{content.textVisualContent}</p>
+                <h2 className="b-textImage__heading">{textVisualHeading}</h2>
+                <p>{textVisualContent}</p>
                 <div className="b-blocks__ctas">
                 {/* {buttons && buttons.length > 0 ?
                     Object.entries(buttons).map((btn, i) => {
