4a7f15fe9640fbfd9c3d8b1ab0bf2e2a370d902f
Ronan <roleary81@gmail.com>
2022-08-08 11:18:05 +0200

    basic typescript set up

--- components/Speaker.tsx
+++ components/Speaker.tsx
@@ -1,13 +1,23 @@
 import useModal from '../hooks/useModal';
 import SpeakerModal from './SpeakerModal';
   
-const Speaker = ({ speaker }) => {
+
+interface ISpeaker {
+    speaker: any
+    speakerName?: string
+    speakerBio?: string
+    speakerCompany?: string
+}
+
+  
+const Speaker = ({ speaker }: ISpeaker ) => {
+    
     const { speakerName, speakerBio, speakerCompany } = speaker;
     const { show, toggleVisibility } = useModal();
   
     return(
         <li className="o-grid__col xs:o-grid__col--span-6 m:o-grid__col--span-3 l:o-grid__col--span-2">
-            <article className="b-speaker" itemType="http://schema.org/Person" itemProp="performer" itemScope="">
+            <article className="b-speaker" itemType="http://schema.org/Person" itemProp="performer">
                 <a className="b-speaker__trigger js-xhrModal" onClick={toggleVisibility}></a>
                 <div className="b-speaker__photo">
                     <img alt={speakerName} className="b-speaker__photoImg" itemProp="image" loading="lazy" src="https://placedog.net/184/230" />
