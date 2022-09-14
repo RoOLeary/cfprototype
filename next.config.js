63a80571f07d1682ea5bfcad28b35b9ffe865507
ronan <ronan@thenextweb.com>
2022-09-14 20:16:09 +0200

    whoop

--- next.config.js
+++ next.config.js
@@ -1,9 +1,10 @@
-/** @type {import('next').NextConfig} */
-const withPWA  = require("next-pwa");
-const nextConfig = {
+const withPWA = require('next-pwa')({
+  dest: 'public'
+})
+module.exports = withPWA({
   reactStrictMode: false,
   trailingSlash: false,
-  distDir: '/dist',
+  distDir: '/out',
   i18n: {
     locales: ['en', 'fr', 'de', 'es'],
     defaultLocale: 'en',
@@ -14,47 +15,39 @@ const nextConfig = {
     domains: ['placedog.net', 'cdn0.tnwcdn.com', 'cfprototype.vercel.app', 'cities.tnwcdn.com'],
   },
   pwa: {
-    dest: "public",
+    dest: 'public',
     swSrc: 'service-worker.js',
-    register: true,
-    skipWaiting: true,
-    publicExcludes: [
-      '!robots.txt',
-      '!sitemap.xml.gz',
-    ],
   },
-}
+});
 
-module.exports = nextConfig
-
-module.exports = {
-  exportPathMap: async function (
-      defaultPathMap,
-      { dev, dir, outDir, distDir, buildId }
-  ) {
-      return {
-      '/': { page: '/' },
-      '/about': { page: '/about' },
-      '/posts': { page: '/posts'}
-      }
-  },
-  async headers() {
-    return [
-      {
-        source: '/all-components',
-        headers: [
-          {
-            key: 'x-custom-header',
-            value: 'my custom header value',
-          },
-          {
-            key: 'x-another-custom-header',
-            value: 'my other custom header value',
-          },
-        ],
-      },
-    ]
-  },
-}
+// // module.exports = {
+// //   exportPathMap: async function (
+// //       defaultPathMap,
+// //       { dev, dir, outDir, distDir, buildId }
+// //   ) {
+// //       return {
+// //       '/': { page: '/' },
+// //       '/about': { page: '/about' },
+// //       '/posts': { page: '/posts'}
+// //       }
+// //   },
+// //   async headers() {
+// //     return [
+// //       {
+// //         source: '/all-components',
+// //         headers: [
+// //           {
+// //             key: 'x-custom-header',
+// //             value: 'my custom header value',
+// //           },
+// //           {
+// //             key: 'x-another-custom-header',
+// //             value: 'my other custom header value',
+// //           },
+// //         ],
+// //       },
+// //     ]
+// //   },
+// // }
 
 
