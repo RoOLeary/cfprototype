9cce629a3839e34d654c33af2f72c4aa32a70cd3
ronan <ronan@thenextweb.com>
2022-09-15 12:29:02 +0200

    alrighty. office time

--- next.config.js
+++ next.config.js
@@ -1,53 +1,54 @@
 const withPWA = require('next-pwa')({
-  dest: 'public'
-})
-module.exports = withPWA({
-  reactStrictMode: false,
-  trailingSlash: false,
-  distDir: '/out',
-  i18n: {
-    locales: ['en', 'fr', 'de', 'es'],
-    defaultLocale: 'en',
-  },
-  images: {
-    loader: 'custom',
-    path: '/',
-    domains: ['placedog.net', 'cdn0.tnwcdn.com', 'cfprototype.vercel.app', 'cities.tnwcdn.com'],
-  },
-  pwa: {
-    dest: 'public',
-    swSrc: 'service-worker.js',
-  },
-});
-
-// // module.exports = {
-// //   exportPathMap: async function (
-// //       defaultPathMap,
-// //       { dev, dir, outDir, distDir, buildId }
-// //   ) {
-// //       return {
-// //       '/': { page: '/' },
-// //       '/about': { page: '/about' },
-// //       '/posts': { page: '/posts'}
-// //       }
-// //   },
-// //   async headers() {
-// //     return [
-// //       {
-// //         source: '/all-components',
-// //         headers: [
-// //           {
-// //             key: 'x-custom-header',
-// //             value: 'my custom header value',
-// //           },
-// //           {
-// //             key: 'x-another-custom-header',
-// //             value: 'my other custom header value',
-// //           },
-// //         ],
-// //       },
-// //     ]
-// //   },
-// // }
-
-
+    dest: 'public'
+  })
+  module.exports = withPWA({
+    reactStrictMode: false,
+    trailingSlash: false,
+    distDir: '/out',
+    i18n: {
+      locales: ['en', 'fr', 'de', 'es'],
+      defaultLocale: 'en',
+    },
+    images: {
+      loader: 'custom',
+      path: '/',
+      domains: ['placedog.net', 'cdn0.tnwcdn.com', 'cfprototype.vercel.app', 'cities.tnwcdn.com'],
+    },
+    pwa: {
+      dest: 'public',
+      swSrc: 'service-worker.js',
+    },
+  });
+  
+  // // module.exports = {
+  // //   exportPathMap: async function (
+  // //       defaultPathMap,
+  // //       { dev, dir, outDir, distDir, buildId }
+  // //   ) {
+  // //       return {
+  // //       '/': { page: '/' },
+  // //       '/about': { page: '/about' },
+  // //       '/posts': { page: '/posts'}
+  // //       }
+  // //   },
+  // //   async headers() {
+  // //     return [
+  // //       {
+  // //         source: '/all-components',
+  // //         headers: [
+  // //           {
+  // //             key: 'x-custom-header',
+  // //             value: 'my custom header value',
+  // //           },
+  // //           {
+  // //             key: 'x-another-custom-header',
+  // //             value: 'my other custom header value',
+  // //           },
+  // //         ],
+  // //       },
+  // //     ]
+  // //   },
+  // // }
+  
+  
+  
