9a06640afda5ea1ff07313a41b2158c87c8f810e
Ronan <roleary81@gmail.com>
2022-09-15 09:51:55 +0200

    see if this and the config changes enables deployment on vercel

--- next.config.js
+++ next.config.js
@@ -30,34 +30,4 @@ module.exports = withPWA({
     },
 });
 
-// // module.exports = {
-// //   exportPathMap: async function (
-// //       defaultPathMap,
-// //       { dev, dir, outDir, distDir, buildId }
-// //   ) {
-// //       return {
-// //       '/': { page: '/' },
-// //       '/about': { page: '/about' },
-// //       '/posts': { page: '/posts'}
-// //       }
-// //   },
-// //   async headers() {
-// //     return [
-// //       {
-// //         source: '/all-components',
-// //         headers: [
-// //           {
-// //             key: 'x-custom-header',
-// //             value: 'my custom header value',
-// //           },
-// //           {
-// //             key: 'x-another-custom-header',
-// //             value: 'my other custom header value',
-// //           },
-// //         ],
-// //       },
-// //     ]
-// //   },
-// // }
-
 
